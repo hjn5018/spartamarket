dfaf87bc5cf36b67a15815366da3ef001e6f5c3b
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-15 20:29:48 +0900

    products/ -u products의 CRUD 추가

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.products, name='products'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/update/', views.update, name='update'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Product
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def products(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'products.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    product = Product(title=title, content=content)
+    product.save()
+    return redirect('product_detail', product.id)
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {
+        'product':product
+    }
+    return render(request, 'product_detail.html', context)
+
+
+def delete(request, pk):
+    product = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('products')
+    return redirect('product_detail', product.pk)
+
+
+def edit(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {
+        'product':product
+    }
+    return render(request, 'edit.html', context)
+
+
+def update(request, pk):
+    product = Product.objects.get(id=pk)
+    product.title = request.POST.get('title')
+    product.content = request.POST.get('content')
+    product.save()
+    return redirect('product_detail', product.id)
