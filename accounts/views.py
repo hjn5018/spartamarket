f9acb86b2981de116107da1f43dfa2c243946670
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-16 19:41:29 +0900

    products/urls.py, views.py -u 로그아웃 기능 추가, templates/base.html -u navbar로 로그인, 로그아웃 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-# Create your views here.
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+
+
 def login(request):
-    form = AuthenticationForm()
-    context = {'form':form}
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
     return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
