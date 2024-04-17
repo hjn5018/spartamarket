9b767ca39022d139c5779ae3dd7f0e6d72666afa
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-17 20:26:06 +0900

    댓글 모델과 폼 생성, 댓글 받아서 보여주기

--- products/models.py
+++ products/models.py
@@ -12,3 +12,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_http_methods
 
@@ -30,8 +30,12 @@ def create(request):
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, id=pk)
+    comment_form = CommentForm()
+    comments = product.comments.all()
     context = {
-        'product':product
+        'product':product,
+        'comment_form':comment_form,
+        'comments':comments,
     }
     return render(request, 'products/product_detail.html', context)
 
@@ -58,3 +62,13 @@ def update(request, pk):
         'product':product,
     }
     return render(request, 'products/update.html', context)
+
+@require_POST
+def create_comment(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.product = product
+        comment.save()
+    return redirect('products:product_detail', pk)
