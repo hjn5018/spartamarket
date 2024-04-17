7d3fa3a9ccb38c1fc4a0c41cb6f095f7edfee4ca
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-17 13:39:20 +0900

    accounts/urls.py, views.py, templates/accounts/signup.html -u 회원가입 기능 생성, templates/base.html -u 회원가입 버튼 추가

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.views.decorators.http import require_POST, require_http_methods
-
+from django.contrib.auth.forms import UserCreationForm
 
 @require_http_methods(['GET', 'POST'])
 def login(request):
@@ -24,3 +24,21 @@ def logout(request):
     if request.user.is_authenticated:
         auth_logout(request)
     return redirect('index')
+
+
+@require_http_methods(['GET', 'POST'])
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/signup.html',context)
+
+
