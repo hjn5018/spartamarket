90adb1a996257755817052ed080003a84ccdd2e4
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-04-19 12:38:09 +0900

    class Accounts의 image와 충돌 방지를 위해 class Product의 image field upload_to를 product_images로 변경하고 migrate함

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='product_images/', blank=True)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
