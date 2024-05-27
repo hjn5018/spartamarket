300de77456dbd6e99ed1eef2868cf635c587a222
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-05-27 11:13:11 +0900

    settings.py ALLOWED_HOSTS to []

--- spartamarket/settings.py
+++ spartamarket/settings.py
@@ -25,12 +25,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '13.209.43.236',
-    'localhost',
-    '127.0.0.1'
-    
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
